831d3c5d045dbe7217be88314754b17623c9e4c3
mohur <m.ashrahfi@gmail.com>
2022-02-15 14:19:35 -0800

    Add more Pokedex info and implement Chakra styles

--- client/src/Components/pokeDex/Index.js
+++ client/src/Components/pokeDex/Index.js
@@ -1,18 +1,43 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import { Button } from "@chakra-ui/react";
+import {
+   AspectRatio,
+   Badge,
+   Box,
+   Button,
+   ButtonGroup,
+   Center,
+   Image,
+   Table,
+   TableCaption,
+   Tbody,
+   Td,
+   Tfoot,
+   Th,
+   Thead,
+   Tr,
+} from "@chakra-ui/react";
 
 export const Index = () => {
-   const [pokeDex, setPokeDex] = useState([]);
+   const [pokeDex, setPokeDex] = useState({
+      image: null,
+      name: "",
+      weight: 0,
+      height: 0,
+      base_experience: 0,
+      abilities: [],
+   });
    const [offSet, setOffSet] = useState(1);
 
    function handleOffSet(e) {
-      if (e.target.name === "Next") {
-         setOffSet(offSet + 1);
-      } else if (e.target.name === "Back" && offSet > 1) {
-         setOffSet(offSet - 1);
-      }
+      setTimeout(() => {
+         if (e.target.name === "Next") {
+            setOffSet(offSet + 1);
+         } else if (e.target.name === "Back" && offSet > 1) {
+            setOffSet(offSet - 1);
+         }
+      }, 100);
    }
 
    useEffect(() => {
@@ -21,11 +46,22 @@ export const Index = () => {
             const request = await axios
                .get(`http://localhost:4000/pokeDex?offset=${offSet}`)
                .then((response) => {
-                  const { name, weight, sprites } = response.data;
+                  console.log(response);
+                  const {
+                     name,
+                     weight,
+                     sprites,
+                     height,
+                     base_experience,
+                     abilities,
+                  } = response.data;
                   setPokeDex({
+                     image: sprites.front_default,
                      name: name,
                      weight: weight,
-                     sprites: sprites.front_default,
+                     height: height,
+                     base_experience: base_experience,
+                     abilities: abilities,
                   });
                })
                .catch((error) => {
@@ -37,30 +73,95 @@ export const Index = () => {
       };
       getPokeDex();
    }, [offSet]);
-   console.log(pokeDex);
+
    return (
       <div>
-         <h1>Pokedex</h1>
-         <div>{pokeDex.name}</div>
-         <div>{pokeDex.weight}</div>
-         <img src={pokeDex.sprites} />
-         <div></div>
-         <Button
-            colorScheme="blue"
-            name="Back"
-            disabled={offSet > 1 ? false : true}
-            onClick={(e) => handleOffSet(e)}
-         >
-            Back
-         </Button>
-         <Button
-            colorScheme="blue"
-            name="Next"
-            onClick={(e) => handleOffSet(e)}
-         >
-            Next
-         </Button>
-         {offSet}
+         <Center>
+            <Box
+               maxW="sm"
+               borderWidth="1px"
+               borderRadius="lg"
+               overflow="hidden"
+            >
+               <AspectRatio maxW="100%" ratio={4 / 3}>
+                  <Image
+                     objectFit="cover"
+                     src={pokeDex.image}
+                     alt={pokeDex.name}
+                  />
+               </AspectRatio>
+               <Box p="6">
+                  <Box display="flex" alignItems="baseline">
+                     {/* <Badge borderRadius="full" px="2" colorScheme="teal">
+                     New
+                  </Badge> */}
+                     {/* <Box
+                     color="gray.500"
+                     fontWeight="semibold"
+                     letterSpacing="wide"
+                     fontSize="xs"
+                     textTransform="uppercase"
+                     ml="2"
+                  >
+                     {property.beds} beds &bull; {property.baths} baths
+                  </Box> */}
+                  </Box>
+
+                  <Table variant="striped" colorScheme="teal">
+                     <Tbody>
+                        <Tr>
+                           <Td>Name</Td>
+                           <Td>{pokeDex.name.toUpperCase()}</Td>
+                        </Tr>
+                        <Tr>
+                           <Td>Weight</Td>
+                           <Td>{pokeDex.weight}</Td>
+                        </Tr>
+                        <Tr>
+                           <Td>Height</Td>
+                           <Td>{pokeDex.height} ft.</Td>
+                        </Tr>
+                        <Tr>
+                           <Td>Base EXP</Td>
+                           <Td>{pokeDex.base_experience}</Td>
+                        </Tr>
+                        <Tr>
+                           <Td>Abilities</Td>
+                           <Td>
+                              {pokeDex.abilities
+                                 .map((i) => i.ability.name)
+                                 .join(", ")
+                                 .toUpperCase()}
+                           </Td>
+                        </Tr>
+                     </Tbody>
+                  </Table>
+               </Box>
+               <Center>
+                  <ButtonGroup width="100%">
+                     <Button
+                        width="100%"
+                        size="lg"
+                        colorScheme="blue"
+                        name="Back"
+                        disabled={offSet > 1 ? false : true}
+                        onClick={(e) => handleOffSet(e)}
+                     >
+                        Back
+                     </Button>
+                     <Button
+                        width="100%"
+                        size="lg"
+                        colorScheme="blue"
+                        name="Next"
+                        onClick={(e) => handleOffSet(e)}
+                     >
+                        Next
+                     </Button>
+                  </ButtonGroup>
+               </Center>
+            </Box>
+         </Center>
       </div>
    );
 };
