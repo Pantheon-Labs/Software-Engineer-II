0e809db601b4afda20d2e622122f21cd17923879
mohur <m.ashrahfi@gmail.com>
2022-02-21 15:29:56 -0800

    Polish server responses

--- server/routes/pokeDex.js
+++ server/routes/pokeDex.js
@@ -7,40 +7,28 @@ const axios = require("axios");
 router.get("/", async (req, res) => {
    try {
       const getPokeDexOffset = async () => {
-         try {
-            return await axios.get(
-               `https://pokeapi.co/api/v2/pokemon/${req.query.offset}`
-            );
-         } catch (e) {
-            res.status(404).send(
-               "Not able to fetch the next/previous pokemon."
-            );
-         }
+         return await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${req.query.offset}`
+         );
       };
 
       const pokeDex = await getPokeDexOffset();
-      res.send(pokeDex.data);
-   } catch (e) {
-      res.status(404).send("Not found.");
+      res.status(200).send(pokeDex.data);
+   } catch (err) {
+      res.status(500).send(err);
    }
 });
 
 router.get("/all", async (req, res) => {
    try {
       const getPokeDexOffset = async () => {
-         try {
-            return await axios.get(
-               `https://pokeapi.co/api/v2/pokemon?limit=1000`
-            );
-         } catch (e) {
-            res.status(404).send("Not able to fetch all pokemon.");
-         }
+         return await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=1000`);
       };
 
       const pokeDex = await getPokeDexOffset();
-      res.send(pokeDex.data);
-   } catch (e) {
-      res.status(404).send("GET request unavailable.");
+      res.status(200).send(pokeDex.data);
+   } catch (err) {
+      res.status(500).send(err);
    }
 });
 
