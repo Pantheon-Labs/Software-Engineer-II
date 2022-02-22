490af75bae31cfe8317840ed34cf899f02480c14
knguyen111601 <knguyen111601@gmail.com>
2022-02-22 11:19:21 -0800

    Working collection view page as well as styling

--- src/pages/ShowCollections.tsx
+++ src/pages/ShowCollections.tsx
@@ -3,15 +3,19 @@ import { Image, Text } from '@chakra-ui/react'
 import React, { useState, useContext, useEffect } from "react"
 import { useParams } from "react-router"
 import { GlobalCtx } from "../App"
+import CollectionItems from "../components/CollectionItems"
+import { useMediaQuery } from "@chakra-ui/media-query"
+
 
 const ShowCollection = () => {
 
     const collectionId = useParams().id
     const {gState} = useContext(GlobalCtx)
-    const {user_id, user_pfp, username, id, token, url} = gState
+    const {url} = gState
+    const [isLargerThan600] = useMediaQuery('(max-width: 600px)')
 
     const [collection, setCollection] = useState<any>(null)
-
+    const collectionBackgroundColors = ['linear(to-r, #2193b0, #6dd5ed)', 'linear(to-r, #2980B9, #6DD5FA)', 'linear(to-r, #7F7FD5, #86A8E7, #91EAE4)', 'linear(to-r, #00B4DB, #0083B0)', 'linear(to-r, #74ebd5, #ACB6E5)',  ]
 
     const getCollection = async () => {
         const response = await fetch(`${url}collections/${collectionId}`)
@@ -25,21 +29,54 @@ const ShowCollection = () => {
 
     return (
 
-
-
         <>
 
         {collection ? 
         
-        <Box>
-        <Text>{collection.title}</Text>
-        <Text>{collection.description}</Text>
-        <Text>{collection.user_username}</Text>
-    </Box> 
+        <Box 
+            d="flex"
+            justifyContent="space-evenly"
+            alignItems="center"
+            bgGradient={collectionBackgroundColors[Math.floor(Math.random() * collectionBackgroundColors.length)]}
+            h={isLargerThan600 ? '350px' : '450px'}
+            flexDir={isLargerThan600 ? 'column' : 'row'}
+            w="100%"
+            >
+
+            <Box w="30%" h="100%" d="flex" flexDir="column" alignItems="center" justifyContent="center">
+                <Image 
+                    src={collection.user_pfp} 
+                    boxSize={isLargerThan600 ? "100px" : "300px"}
+                    borderRadius="500px"
+                    border="10px solid lightblue"
+                    />
+                <Text
+                    fontSize="2em"
+                    color="white"
+                    mt={2}
+                >{collection.user_username}</Text>
+            </Box>
+
+            <Box w="30%" h="100%" d="flex" flexDir="column" justifyContent="center" alignItems="center">
+                <Text
+                    fontSize={isLargerThan600 ? "2em" : "4em"}
+                    color="white"
+                    fontWeight="900"
+                    textAlign="center"
+                >{collection.title}</Text>
+                <Text
+                    color="whitesmoke"
+                    fontSize={isLargerThan600 ? "1em" : "1.5em"}
+                >{collection.description}</Text>
+            </Box>
+
+
+
+        </Box> 
     
-        : null}
+        : <h1>Loading...</h1>}
 
-
+        {collection ? <CollectionItems collectionId={collection.id}/> : null}
         </>
     )
 }  
